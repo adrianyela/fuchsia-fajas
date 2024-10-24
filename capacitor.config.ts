8b7a0e3b0a99a871f2c5e6d4190b1f1dcf3ce599
adrianyela <wayelaqunimayor.edu.co>
2024-10-24 10:37:38 -0500

    primer commit 24/10/2024

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fuchsia-fajas',
+  webDir: 'www'
+};
+
+export default config;
